22714fae2439e47a425c039fe79dcc15816b753c
plasware <77521025+plasware@users.noreply.github.com>
2023-07-28 09:41:11 +0800

    Add coarse recall codes.

---
+++ coarse_recall/NLP_coarse_recall/coarse_recall_v2.py
@@ -0,0 +1,87 @@
+import model_clustering
+import copy
+import numpy as np
+import math
+
+import argparse
+
+parser = argparse.ArgumentParser(description="set task")
+parser.add_argument('--task', type=str, default="mnli", help='name of a huggingface dataset')
+args = parser.parse_args()
+
+if args.task == "mnli":
+    with open('leep_score_mnli.txt', 'r') as f:
+        lines = f.readlines()
+        leep = lines[1].split('\t')
+elif args.task == "tweet":
+    with open('leep_score_tweet.txt', 'r') as f:
+        lines = f.readlines()
+        leep = lines[1].split('\t')
+#print(leep)
+print("Task: %s" % args.task)
+leep_exp = [math.exp(float(leep_score)) for leep_score in leep]
+
+model_clustering_instance = model_clustering.Model_Clustering()
+model_clustering_result = model_clustering_instance.do_cluster()
+# print(model_clustering_result)
+
+model_name = model_clustering_instance.models
+
+# step 1: None singleton recall
+print("---------None Singleton Recall----------")
+model_score_avg = copy.deepcopy(model_clustering_instance.model_score_avg)
+model_cluster_none_singleton = []
+proxy_score_none_singleton = np.zeros(len(model_name)).tolist()
+cluster_representatives = []
+for cluster in model_clustering_result:
+    # select representatives for each none singleton cluster
+    if len(cluster) > 1:
+        cluster_representative = -1
+        cluster_max_score = 0
+        for item in cluster:
+            model_cluster_none_singleton.append(item)
+            if cluster_representative == -1:
+                cluster_representative = item
+                cluster_max_score = model_score_avg[item]
+            else:
+                if model_score_avg[item] > cluster_max_score:
+                    cluster_representative = item
+                    cluster_max_score = model_score_avg[item]
+        cluster_representatives.append(cluster_representative)
+        print("%d: %s selected as cluster representative, leep score: %s" % (cluster_representative,
+                                                                             model_name[cluster_representative],
+                                                                             leep[cluster_representative]))
+
+        # calculate proxy score
+        for item in cluster:
+            proxy_score_none_singleton[item] = model_score_avg[item] * leep_exp[item]
+
+print("----result----")
+
+# step 2: Singleton recall
+print("-----------Singleton Recall------------")
+for cluster in model_clustering_result:
+    if len(cluster) == 1:
+        curr_model_idx = cluster[0]
+        curr_model_acc = np.array(model_clustering_instance.model_scores[curr_model_idx])
+        curr_model_score = 0
+        for item in cluster_representatives:
+            representative_acc = np.array(model_clustering_instance.model_scores[item])
+            cos_similarity = curr_model_acc.dot(representative_acc)/(np.linalg.norm(curr_model_acc) * np.linalg.norm(representative_acc))
+            curr_model_score += (cos_similarity * leep_exp[item])
+        curr_model_score /= len(cluster_representatives)
+        proxy_score_none_singleton[curr_model_idx] = curr_model_score * model_score_avg[curr_model_idx]
+
+recall_idx = []
+selected_cnt = 15
+while selected_cnt > 0:
+    max_idx = proxy_score_none_singleton.index(max(proxy_score_none_singleton))
+    recall_idx.append(str(max_idx))
+    print("%d: %s selected by proxy score: %s" % (
+        max_idx, model_name[max_idx], str(proxy_score_none_singleton[max_idx])))
+    selected_cnt -= 1
+    proxy_score_none_singleton[max_idx] *= -1
+
+result = ','.join(recall_idx)
+with open(args.task + "_recall_result.txt", "w") as f:
+    f.write(result)
---
+++ coarse_recall/NLP_coarse_recall/model_clustering.py
@@ -0,0 +1,194 @@
+import sys
+import math
+import numpy as np
+import os
+
+class Model_Clustering:
+    datasets = None
+    models = None
+    model_scores = None
+    model_scores_norm = []
+    model_score_avg = None
+    model_clusters = []
+    max_dataset_scores = []
+    min_dataset_scores = []
+
+    def __init__(self):
+        self.datasets = [line.strip() for line in open('datasets.txt').readlines()]
+        lines = open('matrix.txt').readlines()
+        self.models = [line.strip() for line in lines[0].split('\t')]
+        self.model_scores = []
+
+        for i in range(1, len(lines)):
+            scores = [float(x) for x in lines[i].split('\t')]
+            self.max_dataset_scores.append(np.max(np.array(scores)))
+            self.min_dataset_scores.append(np.min(np.array(scores)))
+            for j in range(len(scores)):
+                if j >= len(self.model_scores):
+                    self.model_scores.append([scores[j]])
+                else:
+                    self.model_scores[j].append(scores[j])
+        self.model_score_avg = [sum(x) / len(x) for x in self.model_scores]
+        self.model_clusters = []
+        for i in range(len(self.model_scores)):
+            self.model_clusters.append([i])
+
+    def sim_score_by_max_avg_error(self, vec1, vec2):
+        errors = []
+        for i in range(len(vec1)):
+            if vec1[i] > 0 and vec2[i] > 0:
+                error = math.fabs(vec1[i] - vec2[i])
+                errors.append(error)
+        if len(errors) <= 5:
+            return 10000000.0
+        errors = sorted(errors, reverse=True)
+        num = min(5, len(errors))
+        total_error = 0.0
+        for i in range(num):
+            total_error = total_error + errors[i]
+        return total_error / num
+
+    def cluster_sim(self, cluster_i, cluster_j):
+        sum_score = 0.0
+        for i in cluster_i:
+            for j in cluster_j:
+                sum_score = sum_score + self.sim_score_by_max_avg_error(self.model_scores[i], self.model_scores[j])
+        return sum_score / (len(cluster_i) * len(cluster_j))
+
+    def do_cluster(self):
+        while True:
+            min_score = 10000
+            min_index_i, min_index_j = -1, -1
+            for i in range(len(self.model_clusters)):
+                cluster_i = self.model_clusters[i]
+                for j in range(i + 1, len(self.model_clusters)):
+                    cluster_j = self.model_clusters[j]
+                    sim_score = self.cluster_sim(cluster_i, cluster_j)
+                    if sim_score < min_score:
+                        min_index_i, min_index_j = i, j
+                        min_score = sim_score
+
+            if min_score <= 0.09:
+                self.model_clusters[min_index_i] = self.model_clusters[min_index_i] + self.model_clusters[min_index_j]
+                del self.model_clusters[min_index_j]
+                #print('merge cluster %d, %d, score: %.2lf, total clusters: %d' % (
+                #    min_index_i, min_index_j, min_score, len(self.model_clusters)))
+            else:
+                break
+
+        print("clustering result:")
+        total_non_singleton_cluster = 0
+        total_size = 0
+        for i in range(len(self.model_clusters)):
+            if len(self.model_clusters[i]) == 1:
+                continue
+            model_name = ''
+
+            model_and_score = []
+            for j in range(len(self.model_clusters[i])):
+                model_and_score.append((self.model_clusters[i][j], self.model_score_avg[j]))
+            model_and_score = sorted(model_and_score, key=lambda x: x[1], reverse=True)
+
+            for j in range(len(model_and_score)):
+                model_name = model_name + ', ' + self.models[model_and_score[j][0]] + ' : ' + str(model_and_score[j][1])
+                # model_name = model_name + ', ' + models[model_and_score[j][0]]
+            total_non_singleton_cluster = total_non_singleton_cluster + 1
+            total_size = total_size + len(self.model_clusters[i])
+            print('model cluster %d, cluster_size: %d, models: %s' % (i, len(self.model_clusters[i]), model_name))
+
+        print("Total non-singleton cluster num: %d" % total_non_singleton_cluster)
+        print("Total non-singleton cluster size: %d" % total_size)
+        print("Total singleton cluster num: %d" % (len(self.model_clusters) - total_non_singleton_cluster))
+        return self.model_clusters
+
+    def silhouette_coefficient(self):
+        sc = []
+        # first get distance matrix for calculation
+        distance_matrix = np.zeros((len(self.models), len(self.models)))
+        for i in range(len(self.models)):
+            for j in range(i):
+                model_score_x = self.model_scores[i]
+                model_score_y = self.model_scores[j]
+                #_x = np.array(model_score_x)
+                #_y = np.array(model_score_y)
+                # distance_matrix[i][j] = distance_matrix[j][i] = np.linalg.norm(_x - _y)
+                distance_matrix[i][j] = distance_matrix[j][i] = self.sim_score_by_max_avg_error(model_score_x, model_score_y)
+        #print(distance_matrix)
+
+        flatten_cluster_member = [i for arr in self.model_clusters for i in arr]
+        for i in range(len(self.model_clusters)):
+            cluster_sc = []
+            #print(self.model_clusters[i])
+            if len(self.model_clusters[i]) > 1:
+                for item in self.model_clusters[i]:
+                    # get average distance within cluster _a
+                    _a = 0
+                    for other_item in self.model_clusters[i]:
+                        _a += distance_matrix[item][other_item]
+                    _a /= (len(self.model_clusters[i]) - 1)
+
+                    # get the closest distance not in cluster _b
+                    _b = 10000
+                    for j in range(len(self.models)):
+                        if j in flatten_cluster_member:
+                            if j not in self.model_clusters[i]:
+                                _b = min(_b, distance_matrix[item][j])
+
+                    _result = (_b - _a) / max(_a, _b)
+                    sc.append(_result)
+                    cluster_sc.append(_result)
+                cluster_sc_result = np.mean(np.array(cluster_sc))
+                print("cluster silhouette_coefficient: %s" % str(cluster_sc_result))
+        sc_result = np.mean(np.array(sc))
+        print("silhouette_coefficient: %s" % str(sc_result))
+        return sc_result
+
+    def cluster_model_similarity(self):
+        for model_score in self.model_scores:
+            model_score_norm = (np.array(model_score) - np.array(self.min_dataset_scores)) / (
+                                    np.array(self.max_dataset_scores) - np.array(self.min_dataset_scores))
+            self.model_scores_norm.append(model_score_norm)
+        #print(self.model_scores_norm)
+        with open("model_scores_norm.txt", "w") as f:
+            for score_norm in self.model_scores_norm:
+                norm = []
+                for score in score_norm:
+                    norm.append(str(score))
+                str_norm = "\t".join(norm) + "\n"
+                f.write(str_norm)
+
+        for cluster in self.model_clusters:
+            if len(cluster) > 1:
+                similarities = []
+                for i in range(len(cluster)):
+                    for j in range(i):
+                        vec1 = np.array(self.model_scores[cluster[i]])
+                        vec2 = np.array(self.model_scores[cluster[j]])
+                        vec1 = (vec1 - np.array(self.min_dataset_scores)) / (
+                                    np.array(self.max_dataset_scores) - np.array(self.min_dataset_scores))
+                        vec2 = (vec2 - np.array(self.min_dataset_scores)) / (
+                                    np.array(self.max_dataset_scores) - np.array(self.min_dataset_scores))
+                        similarity = vec1.dot(vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+                        similarities.append(similarity)
+                similarity_avg = np.mean(np.array(similarities))
+                print("cluster similarity: %s" % str(similarity_avg))
+
+    def cluster_model_similarity_test(self):
+        for cluster in self.model_clusters:
+            if len(cluster) > 1:
+                similarities = []
+                for i in range(len(cluster)):
+                    for j in range(i):
+                        max_avg_error = self.sim_score_by_max_avg_error(self.model_scores[cluster[i]], self.model_scores[cluster[j]])
+                        similarity = 1 - max_avg_error
+                        similarities.append(similarity)
+                similarity_avg = np.mean(np.array(similarities))
+                print("cluster similarity: %s" % str(similarity_avg))
+
+if __name__ == '__main__':
+    model_cluster_instance = Model_Clustering()
+    model_cluster_instance.do_cluster()
+    model_cluster_instance.cluster_model_similarity()
+    model_cluster_instance.silhouette_coefficient()
+    model_cluster_instance.cluster_model_similarity_test()
+
